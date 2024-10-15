4b63b758ef69cbba404ce7c51951b4f83797a7dd
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-16 01:38:12 +0800

    fix button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -94,7 +94,7 @@ function Sidebar(props: PropsInterface) {
                     </div>
                 </div>
             </div>
-            <button className="flex gap-3 p-3 items-center rounded-md border-2 border-black bg-primary hover:shadow-[-5px_5px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 ease-in-out hover:translate-x-1 hover:-translate-y-1 hover:shadow-none">
+            <button className="flex gap-3 p-3 items-center rounded-md border-2 border-black bg-primary transition-all duration-200 ease-in-out hover:translate-x-1 hover:-translate-y-1 hover:shadow-[-5px_5px_0px_0px_rgba(0,0,0,1)]">
                 <Icon
                     icon="material-symbols:add-location-outline"
                     className="w-6 h-6"
