2435c163504a343ac666027829daa568053b8477
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-16 15:11:42 +0800

    fix sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -92,53 +92,57 @@ function Sidebar(props: PropsInterface) {
                     </div>
                 </div>
             </div>
-            <nav
-                className={`z-0 bg-white w-[320px] h-[calc(100vh-24px)] rounded-md border-2 border-black absolute m-3 p-3 flex flex-col gap-2 transition-transform duration-300 ease-in-out ${
-                    expand ? "translate-x-0" : "-translate-x-[calc(100%+1rem)]"
-                }`}
-            >
-                {/* ignore this, do not remove */}
-                <div className="bg-level-1 bg-level-2 bg-level-3 bg-level-4 bg-level-5 hidden"></div>
-
-                <div className="h-[60px] min-h-[60px]"></div>
-                <button
-                    onClick={pickLocation}
-                    className="flex gap-3 p-3 items-center rounded-md border-2 border-black bg-primary transition-all duration-100 ease-in-out hover:translate-x-1 hover:-translate-y-1 hover:shadow-[-5px_5px_0px_0px_rgba(0,0,0,1)]"
+            <div className="absolute h-lvh p-4">
+                <nav
+                    className={`z-0 bg-white w-[320px] h-full rounded-md border-2 border-black p-3 flex flex-col gap-2 transition-transform duration-300 ease-in-out ${
+                        expand
+                            ? "translate-x-0"
+                            : "-translate-x-[calc(100%+1rem)]"
+                    }`}
                 >
-                    <Icon
-                        icon="material-symbols:add-location-outline"
-                        className="w-6 h-6"
-                    />
-                    <p className="font-medium">Add annotation</p>
-                </button>
-                <div className="flex flex-col gap-6 overflow-y-auto">
-                    {data.map((set, index) => (
-                        <div key={index} className="flex flex-col gap-2">
-                            <div className="p-2">
-                                <p className="font-medium text-slate-600">
-                                    {set.date}
-                                </p>
-                            </div>
-                            {set.annotations.map((annotation, index) => (
-                                <button
-                                    key={index}
-                                    className="rounded-md border border-transparent p-2 flex gap-2 items-center w-full hover:bg-primary-light hover:border hover:border-primary"
-                                >
-                                    <div
-                                        className={`rounded-md w-6 h-6 border-2 border-black bg-level-${annotation.level}`}
-                                    ></div>
-                                    <p className="truncate max-w-[230px]">
-                                        {annotation.name}
+                    {/* ignore this, do not remove */}
+                    <div className="bg-level-1 bg-level-2 bg-level-3 bg-level-4 bg-level-5 hidden"></div>
+
+                    <div className="h-[60px] min-h-[60px]"></div>
+                    <button
+                        onClick={pickLocation}
+                        className="flex gap-3 p-3 items-center rounded-md border-2 border-black bg-primary transition-all duration-100 ease-in-out hover:translate-x-1 hover:-translate-y-1 hover:shadow-[-5px_5px_0px_0px_rgba(0,0,0,1)]"
+                    >
+                        <Icon
+                            icon="material-symbols:add-location-outline"
+                            className="w-6 h-6"
+                        />
+                        <p className="font-medium">Add annotation</p>
+                    </button>
+                    <div className="flex flex-col gap-6 overflow-y-auto">
+                        {data.map((set, index) => (
+                            <div key={index} className="flex flex-col gap-2">
+                                <div className="p-2">
+                                    <p className="font-medium text-slate-600">
+                                        {set.date}
                                     </p>
-                                </button>
-                            ))}
-                        </div>
-                    ))}
-                </div>
-                <button className="text-slate-600 hover:text-primary-dark duration-100">
-                    View all annotations
-                </button>
-            </nav>
+                                </div>
+                                {set.annotations.map((annotation, index) => (
+                                    <button
+                                        key={index}
+                                        className="rounded-md border border-transparent p-2 flex gap-2 items-center w-full hover:bg-primary-light hover:border hover:border-primary"
+                                    >
+                                        <div
+                                            className={`rounded-md w-6 h-6 border-2 border-black bg-level-${annotation.level}`}
+                                        ></div>
+                                        <p className="truncate max-w-[230px]">
+                                            {annotation.name}
+                                        </p>
+                                    </button>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                    <button className="text-slate-600 hover:text-primary-dark duration-100">
+                        View all annotations
+                    </button>
+                </nav>
+            </div>
         </div>
     );
 }
