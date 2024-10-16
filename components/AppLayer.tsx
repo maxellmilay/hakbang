2a4903a55bc8780643e244a2a3493c3c88b61c3d
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-16 19:01:16 +0800

    fix responsiveness

--- components/AppLayer.tsx
+++ components/AppLayer.tsx
@@ -67,7 +67,7 @@ const AppLayer = () => {
             </div>
             <SearchBar isMobile={isMobile} />
             {!isPickingLocation ? (
-                <div className="absolute right-0 bottom-0 p-4 pointer-events-auto">
+                <div className="absolute z-50 right-0 bottom-0 p-4 pointer-events-auto">
                     <button
                         onClick={pickLocation}
                         className="flex items-center justify-center p-3 bg-primary border-2 border-black rounded-full transition-all duration-100 ease-in-out hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]"
