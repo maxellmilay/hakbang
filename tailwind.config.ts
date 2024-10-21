3e6a9ee21e580ee734bc74a5ac323f563d97ff39
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-21 10:52:03 +0800

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend_coordinates: {
+        extend: {
             colors: {
                 background: 'var(--background)',
                 foreground: 'var(--foreground)',
