bc1824b027f41e91d004c6ec8a2fc69f896691c1
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-22 17:28:17 +0800

    remove comment

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,3 @@
-// app/api/upload/route.ts
 import { NextRequest, NextResponse } from 'next/server'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
