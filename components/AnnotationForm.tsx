b354448087e2dcedb6b0cc47cfd815bc7e967cc6
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-11-11 00:07:09 +0800

    fix stoopid typescript

--- components/AnnotationForm.tsx
+++ components/AnnotationForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
 import React, { useEffect, useState } from 'react'
@@ -21,7 +22,6 @@ import RadioItem from './RadioItem'
 
 interface PropsInterface {
     setShowAnnotationForm: (show: boolean) => void
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     saveAnnotation: (lineSegment: MapLineSegment, locationId: number) => void
     pickedCoordinates: {
         latitude: number
@@ -46,7 +46,6 @@ function AnnotationForm(props: PropsInterface) {
 
     const {
         pickedCoordinates,
-        setShowAnnotationForm,
         saveAnnotation,
         pickedLineSegment,
         previousAnnotationData,
--- components/AppLayer.tsx
+++ components/AppLayer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 // React and related
 import { useState, useEffect, Dispatch, SetStateAction } from 'react'
