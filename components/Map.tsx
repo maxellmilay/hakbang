354872a80ed2ca5e842d1c6c23b48f85df711c3e
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-11-15 00:13:38 +0800

    revert lol

--- components/Map.tsx
+++ components/Map.tsx
@@ -83,39 +83,47 @@ const MapComponent = (props: PropsInterface) => {
 
             const fetchLocations = async () => {
                 let page = 1
-                const allScores: AccessibilityScoreData[] = []
-
+                let accessibilityScores = [] as AccessibilityScoreData[]
                 while (true) {
                     try {
                         const res = await getLocations({ page })
-
-                        const newScores = res.objects.map(
-                            (lineSegment: MapLineSegment) => ({
-                                score: lineSegment.accessibility_score,
-                                start_coordinates: {
-                                    latitude:
-                                        lineSegment.start_coordinates.latitude,
-                                    longitude:
-                                        lineSegment.start_coordinates.longitude,
-                                },
-                                end_coordinates: {
-                                    latitude:
-                                        lineSegment.end_coordinates.latitude,
-                                    longitude:
-                                        lineSegment.end_coordinates.longitude,
-                                },
-                            })
-                        )
-
-                        allScores.push(...newScores)
-                        setAccessibilityScores(allScores)
-
+                        accessibilityScores = [
+                            ...accessibilityScores,
+                            ...res.objects.map(
+                                (lineSegment: MapLineSegment) => {
+                                    const parsedLineSegment: AccessibilityScoreData =
+                                        {
+                                            score: lineSegment.accessibility_score,
+                                            start_coordinates: {
+                                                latitude:
+                                                    lineSegment
+                                                        .start_coordinates
+                                                        .latitude,
+                                                longitude:
+                                                    lineSegment
+                                                        .start_coordinates
+                                                        .longitude,
+                                            },
+                                            end_coordinates: {
+                                                latitude:
+                                                    lineSegment.end_coordinates
+                                                        .latitude,
+                                                longitude:
+                                                    lineSegment.end_coordinates
+                                                        .longitude,
+                                            },
+                                        }
+                                    return parsedLineSegment
+                                }
+                            ),
+                        ]
+                        setAccessibilityScores(accessibilityScores)
                         if (res.current === res.num_pages) {
                             break
                         }
                         page++
                     } catch (e) {
-                        console.error('Error fetching locations:', e)
+                        console.error(e)
                         break
                     }
                 }
