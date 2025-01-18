a9045d2cc078cb9b91101663f356e23e8fd9a1cf
Maxell Milay <milaymaxell@gmail.com>
2025-01-18 12:02:31 +0800

    [Fix][Maxell] Updated base url

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { parseCookies, setCookie, destroyCookie } from 'nookies'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/'
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 const api = axios.create({
     baseURL: API_URL,
